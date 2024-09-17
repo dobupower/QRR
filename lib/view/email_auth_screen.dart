84209dcf6a9f104a7d3cb08da1acf5852e24da30
dobupower <bbg999123@naver.com>
2024-09-17 18:50:24 +0900

    UI 상대값으로 변경

--- lib/view/email_auth_screen.dart
+++ lib/view/email_auth_screen.dart
@@ -8,12 +8,14 @@ class EmailAuthScreen extends ConsumerWidget {
   final User user; // 이메일 인증 대상인 사용자
 
   // 생성자에서 필수적으로 User 객체를 받아옴
-  
   EmailAuthScreen({required this.user});
 
   @override
-  
   Widget build(BuildContext context, WidgetRef ref) {
+    // 화면 크기 정보 가져오기
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+
     // SignUpState 및 SignUpViewModel 상태를 감시
     final signUpState = ref.watch(signUpViewModelProvider); 
     final signUpViewModel = ref.read(signUpViewModelProvider.notifier);
@@ -29,7 +31,7 @@ class EmailAuthScreen extends ConsumerWidget {
       ),
       // 본문 내용
       body: Padding(
-        padding: const EdgeInsets.all(16.0), // 화면 여백
+        padding: EdgeInsets.all(screenWidth * 0.05), // 화면 여백을 상대적으로 설정
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start, // 왼쪽 정렬
           children: [
@@ -38,35 +40,36 @@ class EmailAuthScreen extends ConsumerWidget {
               child: Text(
                 'アカウント認証', // 'アカウント認証' = 계정 인증
                 style: TextStyle(
-                  fontSize: 24,
+                  fontSize: screenWidth * 0.07, // 폰트 크기를 상대적으로 설정
                   fontWeight: FontWeight.bold,
                 ),
               ),
             ),
-            SizedBox(height: 16), // 간격 추가
+            SizedBox(height: screenHeight * 0.02), // 간격을 상대적으로 설정
             // 사용자 이메일 정보와 안내 메시지
             Center(
               child: Text(
                 '${user.email}に認証コードを送りました。\n認証コードを入力してください。', 
                 // '認証コードを送りました。' = 인증 코드를 보냈습니다.
                 textAlign: TextAlign.center, // 텍스트 가운데 정렬
-                style: TextStyle(fontSize: 16, color: Colors.black),
+                style: TextStyle(fontSize: screenWidth * 0.038, color: Colors.black), // 텍스트 크기를 상대적으로 설정
               ),
             ),
-            SizedBox(height: 32), // 간격 추가
+            SizedBox(height: screenHeight * 0.04), // 간격을 상대적으로 설정
             // 인증 코드 입력 필드 라벨
             Text(
               '認証コード', // '認証コード' = 인증 코드
-              style: TextStyle(fontSize: 16),
+              style: TextStyle(fontSize: screenWidth * 0.045), // 폰트 크기를 상대적으로 설정
             ),
-            SizedBox(height: 8), // 간격 추가
+            SizedBox(height: screenHeight * 0.01), // 간격을 상대적으로 설정
             // 인증 코드 입력 필드
             TextField(
               controller: signUpState.codeController, // 인증 코드 입력을 위한 컨트롤러
               decoration: InputDecoration(
                 hintText: '4桁コードを入力', // '4桁コードを入力' = 4자리 코드를 입력
                 border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0), // 필드 테두리 둥글게 설정
+                  borderRadius: BorderRadius.circular(screenWidth * 0.03), // 필드 테두리를 상대적으로 설정
+                  borderSide: BorderSide(color: Colors.grey),
                 ),
               ),
               keyboardType: TextInputType.number, // 숫자 입력만 가능하도록 설정
@@ -85,7 +88,7 @@ class EmailAuthScreen extends ConsumerWidget {
                 },
                 child: Text(
                   'コード再送する', // 'コード再送する' = 코드 재전송
-                  style: TextStyle(color: Colors.blue, fontSize: 14),
+                  style: TextStyle(color: Colors.blue, fontSize: screenWidth * 0.04), // 텍스트 크기를 상대적으로 설정
                 ),
               ),
             ),
@@ -102,18 +105,18 @@ class EmailAuthScreen extends ConsumerWidget {
                 },
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Color(0xFF1D2538), // 버튼 배경색 설정
-                  padding: EdgeInsets.symmetric(horizontal: 120, vertical: 12), // 버튼 크기 설정
+                  padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.3, vertical: screenHeight * 0.015), // 버튼 크기를 상대적으로 설정
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50), // 버튼 모서리를 둥글게 설정
+                    borderRadius: BorderRadius.circular(screenWidth * 0.07), // 버튼 모서리를 둥글게 상대적으로 설정
                   ),
                 ),
                 child: Text(
                   'アカウント認証', // 'アカウント認証' = 계정 인증
-                  style: TextStyle(fontSize: 18, color: Colors.white),
+                  style: TextStyle(fontSize: screenWidth * 0.045, color: Colors.white), // 텍스트 크기를 상대적으로 설정
                 ),
               ),
             ),
-            SizedBox(height: 16), // 마지막 간격
+            SizedBox(height: screenHeight * 0.05), // 마지막 간격을 상대적으로 설정
           ],
         ),
       ),
