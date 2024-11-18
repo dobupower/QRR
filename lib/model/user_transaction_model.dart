a31ac899c6c60eb848e058434f673cbd11d5d67a
dobupower <bbg999123@naver.com>
2024-11-18 15:26:19 +0900

    로그인 회원가입 로직변경

---
+++ lib/model/user_transaction_model.dart
@@ -0,0 +1,30 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'user_transaction_model.freezed.dart';
+part 'user_transaction_model.g.dart';
+
+@freezed
+class UserTransaction with _$UserTransaction {
+  const factory UserTransaction({
+    required String transactionId,       // 트랜잭션 고유 ID
+    required String senderUid,         // 보내는 사람 이메일
+    required String receiverUid,       // 받는 사람 이메일
+    required int amount,              // 거래 금액
+    required DateTime timestamp,         // 거래 발생 시각
+  }) = _UserTransaction;
+
+  // Firestore 데이터와 변환할 때 사용할 JSON 직렬화 메서드
+  factory UserTransaction.fromJson(Map<String, dynamic> json) => _$UserTransactionFromJson(json);
+}
+extension UserTransactionExtensions on UserTransaction {
+  // Firestore에 저장할 때 변환할 Map 형식
+  Map<String, dynamic> toFirestore() {
+    return {
+      'transactionId': transactionId,
+      'senderUid': senderUid,
+      'receiverUid': receiverUid,
+      'amount': amount,
+      'timestamp': timestamp.toIso8601String(),
+    };
+  }
+}
